34726df0351e91d797f6316cfbdb25ccdd7e9440
alotofsnakes <mrfries@gmail.com>
2022-09-26 20:41:37 -0700

    modularized routes

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
